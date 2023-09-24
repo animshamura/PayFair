42e69beb404716d8ed0ac15be160f79331c7cec6
Shamura Ahmad Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-09-24 15:28:29 +0600

    Add files via upload

---
+++ PayFair/PayFair/Form1.cs
@@ -0,0 +1,15 @@
+namespace PayFair
+{
+    public partial class PayFair : Form
+    {
+        public PayFair()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
